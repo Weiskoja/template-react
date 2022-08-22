e78ca8149a3b1964a7a6b1b88cc2b04b22b10fd9
bnjmnm <benm@umich.edu>
2022-08-22 06:02:03 -0400

    calmed down linter

--- cypress/e2e/e2eTest.cy.js
+++ cypress/e2e/e2eTest.cy.js
@@ -1,4 +1,17 @@
-describe('Midterm1', () => {
+// Monitor console warnings, errors, and logs
+let consoleError;
+let consoleWarning;
+let consoleLog;
+
+
+Cypress.on('window:before:load', (win) => {
+  consoleError = cy.spy(win.console, 'error');
+  consoleWarning = cy.spy(win.console, 'warn');
+  consoleLog = cy.spy(win.console, 'log');
+});
+const DELAY = 1000;
+
+describe('Basic check', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
@@ -10,5 +23,14 @@ describe('Midterm1', () => {
     cy.get('header').first().should('include.text', 'Learn React')
   })
 
-
+  afterEach(() => {
+    // Confirm there are no console log/warning/errors after every test iteration.
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(DELAY).then(() => {
+      /* eslint-disable no-unused-expressions */
+      expect(consoleError).to.not.be.called;
+      expect(consoleWarning).to.not.be.called;
+      expect(consoleLog).to.not.be.called;
+    });
+  });
 })
