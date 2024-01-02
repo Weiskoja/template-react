db716b396708c32c83d0940d1b53a33de1a47bb6
Mullins, Benjamin <benm@umich.edu>
2024-01-02 10:15:54 -0500

    cleanup

--- cypress/component/componentTests.cy.js
+++ cypress/component/componentTests.cy.js
@@ -5,16 +5,11 @@ import 'cypress-react-selector'
 
 describe('component tests', () => {
   beforeEach(() => {
-    // cy.visit('http://localhost:3000/myApp');
     mount(<App />);
     cy.waitForReact(1000, '#root');
-// 1000 is the timeout in milliseconds, you can provide as per AUT
   });
-  // it('has what you would expect from default create react app', () => {
-  //   mount(<App />);
-  // })
-  it('App has a single <Comp> Component', () => {
 
+  it('App has a single <TestComponent> Component', () => {
     cy.react('TestComponent').should('have.length', 1 )
   })
 })
